a4d34068fb78fe38a5bd2958c8737c9825393a4d
Michael Whittaker <mjw297@cornell.edu>
2017-04-26 08:39:20 -0700

    Minor fixes of setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name="gym_foo",
+setup(name="gym_hanabi",
       version="0.0.1",
       install_requires=[
           "gym",
           "numpy",
+          "six",
           "termcolor",
       ]
 )
