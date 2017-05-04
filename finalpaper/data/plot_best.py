abaf6e69ca1e9b95ab75e9d31de9c7edfadcc685
Michael Whittaker <mjw297@cornell.edu>
2017-05-04 10:22:37 -0700

    Removed spaces for real this time.

--- finalpaper/data/plot_best.py
+++ finalpaper/data/plot_best.py
@@ -23,7 +23,9 @@ def hist(name, bins, learning, filename):
     plt.ylabel("Frequency")
     plt.title("TRPO {} {} policy\nmean={}, stddev={:0.4}"
               .format(learning, name, mean, stddev))
-    plt.savefig("{}_{}.pdf".format(name, learning.replace(" ", "_")), bbox_inches="tight")
+    plt.savefig("{}_{}.pdf".format(name.replace(" ", "_"),
+                                   learning.replace(" ", "_")),
+                                   bbox_inches="tight")
     plt.close()
 
 def main():
