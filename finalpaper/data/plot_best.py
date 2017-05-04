d6c4d636610b94032db7886ed4b906b95677001c
Michael Whittaker <mjw297@cornell.edu>
2017-05-04 10:27:57 -0700

    Added colors to best plots.

--- finalpaper/data/plot_best.py
+++ finalpaper/data/plot_best.py
@@ -7,7 +7,7 @@ import numpy as np
 font = {"size": 16}
 matplotlib.rc("font", **font)
 
-def hist(name, bins, learning, filename):
+def hist(name, bins, learning, color, filename):
     # 0: length
     # 1: reward
     # 2: score
@@ -17,7 +17,8 @@ def hist(name, bins, learning, filename):
     stddev = np.std(scores)
 
     plt.figure()
-    plt.hist(data[:,2].astype(np.int), edgecolor="black", align="left", bins=range(bins))
+    plt.hist(data[:,2].astype(np.int), color=color, edgecolor="black",
+             align="left", bins=range(bins))
     plt.xticks(range(0, bins - 1, 1 if bins <= 11 else 2))
     plt.xlabel("Score")
     plt.ylabel("Frequency")
@@ -34,19 +35,19 @@ def main():
     bins = 27
 
     policies = [
-        ("mini Hanabi",   mini_bins,   "dynamic self-learned", "../../gym_hanabi/policies/logs/mini_trpo_self_best.txt"),
-        ("medium Hanabi", medium_bins, "dynamic self-learned", "../../gym_hanabi/policies/logs/medium_trpo_self_best.txt"),
-        ("Hanabi",        bins,        "dynamic self-learned", "../../gym_hanabi/policies/logs/trpo_self_best.txt"),
-        ("mini Hanabi",   mini_bins,   "guided-learned",       "../../gym_hanabi/policies/logs/mini_trpo_ai_best.txt"),
-        ("medium Hanabi", medium_bins, "guided-learned",       "../../gym_hanabi/policies/logs/medium_trpo_ai_best.txt"),
-        ("Hanabi",        bins,        "guided-learned",       "../../gym_hanabi/policies/logs/trpo_ai_best.txt"),
-        ("mini Hanabi",   mini_bins,   "static self-learned",  "../../gym_hanabi/policies/logs/mini_trpo_staggered_best.txt"),
-        ("medium Hanabi", medium_bins, "static self-learned",  "../../gym_hanabi/policies/logs/medium_trpo_staggered_best.txt"),
-        ("Hanabi",        bins,        "static self-learned",  "../../gym_hanabi/policies/logs/trpo_staggered_best.txt"),
+        ("mini Hanabi",   mini_bins,   "dynamic self-learned", "C0", "../../gym_hanabi/policies/logs/mini_trpo_self_best.txt"),
+        ("medium Hanabi", medium_bins, "dynamic self-learned", "C0", "../../gym_hanabi/policies/logs/medium_trpo_self_best.txt"),
+        ("Hanabi",        bins,        "dynamic self-learned", "C0", "../../gym_hanabi/policies/logs/trpo_self_best.txt"),
+        ("mini Hanabi",   mini_bins,   "guided-learned",       "C1", "../../gym_hanabi/policies/logs/mini_trpo_ai_best.txt"),
+        ("medium Hanabi", medium_bins, "guided-learned",       "C1", "../../gym_hanabi/policies/logs/medium_trpo_ai_best.txt"),
+        ("Hanabi",        bins,        "guided-learned",       "C1", "../../gym_hanabi/policies/logs/trpo_ai_best.txt"),
+        ("mini Hanabi",   mini_bins,   "static self-learned",  "C2", "../../gym_hanabi/policies/logs/mini_trpo_staggered_best.txt"),
+        ("medium Hanabi", medium_bins, "static self-learned",  "C2", "../../gym_hanabi/policies/logs/medium_trpo_staggered_best.txt"),
+        ("Hanabi",        bins,        "static self-learned",  "C2", "../../gym_hanabi/policies/logs/trpo_staggered_best.txt"),
     ]
 
-    for name, bins, learning, filename in policies:
-        hist(name, bins, learning, filename)
+    for args in policies:
+        hist(*args)
 
 if __name__ == "__main__":
     main()
