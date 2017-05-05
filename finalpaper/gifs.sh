848897b694822980739eb7c9cc97a07b7b657a13
Stephanie <swang@cs.berkeley.edu>
2017-05-04 21:50:33 -0700

    RL gifs

---
+++ finalpaper/gifs.sh
@@ -0,0 +1,3 @@
+wget -O pong.mp4 https://media.giphy.com/media/BmRMiTaeWFgd2/giphy.mp4
+wget -O go.mp4 https://media.giphy.com/media/fBnxS9yMW3h96/giphy.mp4
+wget -O cartpole.mp4 https://openai-kubernetes-prod-scoreboard.s3.amazonaws.com/v1/evaluations/eval_lEi8I8v2QLqEgzBxcvRIaA/training_episode_batch_video.mp4
